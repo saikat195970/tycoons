dcbbe7cb38bf12d9c073ee3da1377d51e6421609
“Saikat <saikatpramanik007@live.com>
2023-02-12 00:18:03 +0530

    initial commit

---
+++ src/main/java/com/saikat/tycoons/TycoonsApplication.java
@@ -0,0 +1,13 @@
+package com.saikat.tycoons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TycoonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TycoonsApplication.class, args);
+	}
+
+}
